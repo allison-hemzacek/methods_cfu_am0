679a4b588921c9c5f7bf48368591b114e8a338ea
Allison Hemzacek <allison.hemzacek@gmail.com>
2022-04-22 14:01:46 -0600

    After end of zoom

--- final_practice.rb
+++ final_practice.rb
@@ -3,26 +3,31 @@
 def greeting
   puts "Go away"
 end
-
 # What is the return value of your method?
+"Go away"
 # How many arguments did you pass your method?
-
+0
 
 
 
 #2: Write a method named custom_greeting that prints out a greeting WITH a specific name.
+def custom_greeting(name1)
+  puts "Hello " + name1
+end
 
+custom_greeting ("Sterling")
 
 # What is the return value of your method?
 # How many arguments did you pass your method?
 # What data type was your argument(s)?
 
 
-
-
 #3: Write a method named square that takes in one number, and returns the square of that number
+def square (num)
+  puts num * num
+end
 
-
+square (8)
 
 # What is the return value of your method?
 # How many arguments did you pass your method?
@@ -32,7 +37,11 @@ end
 
 
 #4: Write a method named full name that takes in 3 strings, a first, middle, and last name, and print outs the sentence of the entire string
+def full_name (first, middle, last)
+  puts "It is so nice to work with you " + first + middle + last
+end
 
+full_name("Anna ", "Marie ", "Sterling ")
 
 
 # What is the return value of your method?
